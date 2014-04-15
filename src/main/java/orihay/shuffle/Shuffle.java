c1270c5d003df721cfb228c3795f572cd82f95ca
jyahiro <yahiro_john@bah.com>
2014-04-14 23:14:37 -0400

    beginning to implement merge sort

--- src/main/java/orihay/shuffle/Shuffle.java
+++ src/main/java/orihay/shuffle/Shuffle.java
@@ -1,9 +1,53 @@
 package orihay.shuffle;
 
-public class Shuffle {
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+import orihay.cards.Card;
+import orihay.cards.Deck;
+
+public class Shuffle {
+
+  public static Deck withRand(Deck d, long seed) {
+    List<Card> cards = d.getCards();
+    if (cards.size() > 1) {
+      sort(cards, seed);
+    }
+    merge(cards, seed);
+    return d;
   }
 
+  private static List<Card> merge(List<Card> first, List<Card> second, long seed) {
+
+    if (cards.size() == 2) {
+      return sort(cards.get(0), cards.get(1), seed);
+    } else {
+      return merge(cards, seed);
+    }
+  }
+
+  private static List<Card> sort(List<Card> cards, long seed) {
+    int floor = (int) Math.floor(cards.size() / 2);
+    List<Card> first = cards.subList(0, floor);
+    List<Card> second = cards.subList(floor + 1, cards.size() - 1);
+    merge(sort(first, seed), sort(second, seed), seed);
+  }
+
+  private static List<Card> sort(Card one, Card two, long seed) {
+    ArrayList<Card> sorted = new ArrayList<Card>();
+    if (decide(seed)) {
+      sorted.add(one);
+      sorted.add(two);
+    } else {
+      sorted.add(two);
+      sorted.add(one);
+    }
+    return sorted;
+  }
+
+  private static boolean decide(long seed) {
+    Random random = new Random(seed);
+    return random.nextBoolean();
+  }
 }
